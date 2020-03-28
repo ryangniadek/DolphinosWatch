26264f41a0c97bddef598fd55f5e1246b2da2b5f
Ryan Gniadek <ryan@gniadek.net>
2020-03-28 17:37:04 -0400

    Added help pages

--- bot.py
+++ bot.py
@@ -11,17 +11,22 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 bot = commands.Bot(command_prefix='DW!')
 usernames = ['dolphinos', 'LHemisphere', 'neberizer']
+helper = {
+    'in': 'Checks if dolphinos is in a game on any of his accounts. Pass an optional paramter to check for another user.',
+    'time': 'Checks how long dolphinos has been in game',
+    'last': 'Checks when the last game played by Dolphinos was'
+}
 
-@bot.command(name='in')
+@bot.command(name='in', help=helper['in'])
 async def in_command(ctx):
     in_game = False
     for user in usernames:
-        if lol.check_if_ingame(user):
+        if lol.check_if_ingame(user): 
             in_game = True
     response = "@Dolphinos IS PLAYING LEAGUE OF LEGENDS" if in_game else "The neberizer is not playing league. @Dolphinos, time to come online!"
     await ctx.send(response)
 
-@bot.command(name='time')
+@bot.command(name='time', help=helper['time'])
 async def time_command(ctx):
     for user in usernames:
         if lol.check_if_ingame(user):
@@ -29,7 +34,7 @@ async def time_command(ctx):
             return
     await ctx.send("The bbern is not in a game")
 
-@bot.command(name='last')
+@bot.command(name='last', help=helper['last'])
 async def last_command(ctx):
     response = "last game"
     await ctx.send(response)
