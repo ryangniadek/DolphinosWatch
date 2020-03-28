db037d807f138fcaac583a20c776d60fa6162f70
Danny Flynn <pdflynn@vt.edu>
2020-03-28 16:30:56 -0400

    added check_if_ingame

--- LeagueScraper.py
+++ LeagueScraper.py
@@ -16,13 +16,29 @@ cass.set_default_region("NA")
 # functions ----------------------------------------------------------------
 # Returns a boolean value if dolphinos is currently in a game
 def check_if_ingame(summoner):
-    return True
+    try:
+        match = summoner.current_match
+    except:
+        return False
+    else:
+        return True
 
+# Returns the amount of time, in seconds, a summoner has been in a match
+def get_match_time(summoner):
+    return 0
+
+# Returns the level of the specified summoner
 def get_level(summoner):
-    
+    return 0
 
+# Returns a summoner object with the specified name
+def get_summoner(summonerName):
+    return cass.get_summoner(name=summonerName)
 
-# temp execution stuff
+# testing
 
-dolphinos = cass.get_summoner(name="dolphinos")
-print("{name} is a level {level} TURBOVIRGIN on the {region} server".format(name=dolphinos.name, level=dolphinos.level, region=dolphinos.region))
+nightblue = get_summoner("Nightblue3")
+dolphinos = get_summoner("dolphinos")
+
+print("Nightblue3 in game? " + str(check_if_ingame(nightblue)))
+print("dolphinos in game? " + str(check_if_ingame(dolphinos)))
